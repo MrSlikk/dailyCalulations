f9ba7b4f24f9b345041e79770b24f8f168de1a55
MrSlikk <57304100+MrSlikk@users.noreply.github.com>
2026-09-13 01:08:35 +0300

    Increment cache version to v3
    Updated cache name to version 3 for asset management.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sales-calc-v2'; // Increment this (v1 -> v2) whenever you update files
+const CACHE_NAME = 'sales-calc-v3'; // Increment this (v1 -> v2) whenever you update files
 
 const ASSETS = [
   './',
